f27dc3790ef3380cc1871a9ae8643449a3a297dd
SidorovaAS <39182580+SidorovaAS@users.noreply.github.com>
2018-06-08 09:18:39 +0300

    Add files via upload

---
+++ лаба4.1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A NEWA = new A();
+            Console.WriteLine(NEWA.C);
+            Console.ReadKey();
+        }
+    }
+    class A
+    {
+        private int a;
+        private int b;
+        private int c;
+
+
+        public int C
+        {
+            get
+            {
+                a = 5;
+                b = 2;
+                if (a > b)
+                    c = a * b;
+                else c = a + b;
+
+                return c;
+            }
+            set
+            {
+                c = value;
+            }
+        }
+
+    }
+}
